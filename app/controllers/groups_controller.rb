d262c0ba9cfd14ff6477bcaebbd6b29268f2df5a
liubingjie <breeder1121@hotmail.com>
2017-06-19 14:57:02 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,32 +1,39 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
   end
+
   def new
     @group = Group.new
   end
+
   def show
     @group = Group.find(params[:id])
   end
+
   def edit
     @group = Group.find(params[:id])
   end
+
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
-   if   @group.save
-    redirect_to groups_path
-  else
-    render :new
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
-  end
+
   def update
     @group = Group.find(params[:id])
-  if   @group.update(group_params)
-    redirect_to groups_path, notice: "Update success!"
-  else
-    render :edit
-  end
+    if   @group.update(group_params)
+      redirect_to groups_path, notice: "Update success!"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -34,6 +41,7 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
+
   private
 
   def group_params
